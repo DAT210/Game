04e2cd54ffa94a9d9ee5d0b9e08594ef14aca71d
gauhau <gaute.haugen@lyse.net>
2018-11-03 00:05:21 +0100

    Changed the logger to behave diffrently depending on the environment(dev, test and production)

--- src/tools/logger.js
+++ src/tools/logger.js
@@ -2,32 +2,81 @@ const path = require("path")
 const { createLogger, format, transports } = require("winston");
 require("winston-daily-rotate-file");
 
-module.exports.getLogger = function setupLogger() {
+module.exports.getLogger = function setupLogger(environment) {
 	const logDir = "logs";
-    const temp_path = path.resolve(__dirname, "../tools/", logDir) //Path to logDir where all logs will be saved
+    const temp_path = path.resolve(__dirname, "../", logDir) //Path to logDir where all logs will be saved
 
- 	const dailyRotateFileTransport = new transports.DailyRotateFile({ //Makes a new log document every day
-    		filename: temp_path+`/%DATE%-log.json`,
-    		datePattern: "YYYY-MM-DD"
-  	});
-
-	const logger = createLogger({
-  		level: "info",  //Level is set to info, can change levels if you want to use it to something different feks debug
-  		format: format.combine(
-      		format.colorize(),
-      		format.timestamp({
-          		format: "DD-MM-YYYY HH:mm:ss" //Date and time
-      		}),
-      		format.json()),
-		transports: [ new transports.Console({
-    		level: "info",
-    		format: format.combine(
-      		format.colorize(),
-      		format.printf(
-        		info => `${info.timestamp} : ${info.message}`//How it should look in the log document
-      		))
-  		}),dailyRotateFileTransport] //Transport command to send it tot the chosen log fil
-	});
-
-	return logger;
+	switch(environment){
+		case "dev":
+			// Will add log file everytime you run the server
+			return createLogger({
+				// Setup for what the logger should recieve
+				level: "silly",
+				format: format.combine(
+					format.colorize(),
+					format.timestamp({
+						format: "DD-MM-YYYY HH:mm:ss"
+					}),
+					format.json()
+				),
+				transports: [new transports.Console({
+					// Setup for the console printing
+					level: "silly",
+					format: format.combine(
+						format.colorize(),
+						format.printf(
+							info => `[${info.timestamp}] [${info.level}] ${info.message}`
+						))
+				}), new transports.DailyRotateFile({
+					// Setup for log file
+					filename: temp_path+`/dev-logs/%DATE%-devlog.json`,
+					datePattern: "YYYY-MM-DD"
+				})]
+			});
+		case "test":
+			// Will add log file with information from a spesific test run
+			return createLogger({
+				// Setup for what the logger should recieve
+				level: "silly",
+				format: format.combine(
+					format.colorize(),
+					format.timestamp({
+						format: "DD-MM-YYYY HH:mm:ss"
+					}),
+					format.json()
+				),
+				transports: [new transports.DailyRotateFile({
+					// Setup for log file
+					filename: temp_path+`/test-logs/%DATE%-testlog.json`,
+					datePattern: "YYYY-MM-DD-HH-mm"
+				})]
+			});
+		case "production":
+			// Will add a log file every day
+			return createLogger({
+				// Setup for what the logger should recieve
+				level: "silly",
+				format: format.combine(
+					format.colorize(),
+					format.timestamp({
+						format: "DD-MM-YYYY HH:mm:ss"
+					}),
+				  format.json()
+			  ),
+			  transports: [new transports.Console({
+				  // Setup for the console printing
+				  level: "silly",
+				  format: format.combine(
+						format.colorize(),
+						format.printf(
+						  info => `[${info.timestamp}] [${info.level}] ${info.message}`
+						))
+				}), new transports.DailyRotateFile({
+					// Setup for log file
+					filename: temp_path+`/production-logs/%DATE%-log.json`,
+					datePattern: "YYYY-MM-DD",
+					maxFiles: "31d"
+				})]
+			});
+	}
 }
